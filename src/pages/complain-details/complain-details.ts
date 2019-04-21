a0047f127600559d22296da7b9ad7b94d4740fd6
Harsh Pratyush <harshpratyush1206@gmail.com>
2019-04-22 00:52:39 +0530

    call feature added

--- src/pages/complain-details/complain-details.ts
+++ src/pages/complain-details/complain-details.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { CallNumber } from '@ionic-native/call-number';
 /**
  * Generated class for the ComplainDetailsPage page.
  *
@@ -17,7 +17,7 @@ export class ComplainDetailsPage {
 
   complainStatus:ComplainStatus
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private callNumber: CallNumber) {
   }
 
   ionViewDidLoad() {
@@ -29,5 +29,12 @@ export class ComplainDetailsPage {
     this.complainStatus=this.navParams.data;
     else
     this.navCtrl.setRoot('ComplainStatusPage')
+  }
+
+  callMechanic()
+  {
+    this.callNumber.callNumber(this.complainStatus.mechanicContactNumber.toString(), true)
+    .then(res => console.log('Launched dialer!', res))
+    .catch(err => console.log('Error launching dialer', err));
   }
 }
