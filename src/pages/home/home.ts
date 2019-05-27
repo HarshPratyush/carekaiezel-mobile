61f83578880d6ce2aecabcdda4995d202e53c344
Harsh Pratyush <harshpratyush1206@gmail.com>
2019-05-27 23:22:43 +0530

    Home Page Done

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ConstantsServiceProvider } from '../../providers/constants-service/constants-service';
+import { UtilServiceProvider } from '../../providers/util-service/util-service';
 
 /**
  * Generated class for the HomePage page.
@@ -16,8 +17,11 @@ import { ConstantsServiceProvider } from '../../providers/constants-service/cons
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public constantsService:ConstantsServiceProvider) {
-  }
+  icons: Array<{title: string, component: string,icon:string}>=[];
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public constantsService:ConstantsServiceProvider,public utilServiceProvider:UtilServiceProvider) {
+      this.icons=utilServiceProvider.getMenu().filter(d=>d.component!='HomePage');
+    }
 
   ionViewDidLoad() {
   }
@@ -25,6 +29,18 @@ export class HomePage {
   ngOnInit(){
     if(localStorage.getItem(this.constantsService.USER_DETAILS)==null)
     {
+      this.navCtrl.setRoot('LoginPage');
+    }
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    if(page.component!='Logout')
+    this.navCtrl.setRoot(page.component);
+    else
+    {
+      localStorage.clear();
       this.navCtrl.setRoot('LoginPage');
     }
   }
